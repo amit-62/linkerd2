460b64a283589acc7f17689932b4e4081c869fcd
amit-62 <kramit6662@gmail.com>
2023-05-15 23:56:03 +0530

    stack folded lines
    Signed-off-by: amit-62 <kramit6662@gmail.com>

--- policy-controller/src/profiling.rs
+++ policy-controller/src/profiling.rs
@@ -1,4 +1,4 @@
-use pprof::{protos::Message, ProfilerGuardBuilder};
+use pprof::{protos::Message, ProfilerGuardBuilder, Report};
 use serde::{Deserialize, Serialize};
 use std::sync::Arc;
 use warp::{http::Response, Filter};
@@ -9,6 +9,42 @@ const PPROF_PORT: u16 = 81;
 struct QueryParams {
     format: String,
 }
+
+struct MyReport(Report);
+
+impl MyReport {
+    pub fn folded<W>(&self, mut writer: W) -> Result<()>
+    where
+        W: std::io::Write,
+    {
+        let lines: Vec<String> = self
+            .data
+            .iter()
+            .map(|(key, value)| {
+                let mut line = key.thread_name_or_id();
+                line.push(';');
+
+                for frame in key.frames.iter().rev() {
+                    for symbol in frame.iter().rev() {
+                        write!(&mut line, "{};", symbol).unwrap();
+                    }
+                }
+
+                line.pop().unwrap_or_default();
+                write!(&mut line, " {}", value).unwrap();
+
+                line
+            })
+            .collect();
+
+        for line in lines {
+            writeln!(writer, "{}", line)?;
+        }
+
+        Ok(())
+    }
+
+}    
 
 pub fn init() {
     let guard = Arc::new(
@@ -47,7 +83,15 @@ pub fn init() {
                                 .header("Content-Disposition", "attachment; filename=profile.pb")
                                 .body(file)
                                 .unwrap()
-                        } else {
+                        } else if obj.format == "folded" {
+                            let mut file = Vec::new();
+                            report.folded(&mut file).unwrap();
+                            Response::builder()
+                                .header("content-type", "text/plain")
+                                .body(file)
+                                .unwrap()
+                        }
+                         else {
                             Response::builder()
                                 .body(Vec::from("unknown value for format"))
                                 .unwrap()
